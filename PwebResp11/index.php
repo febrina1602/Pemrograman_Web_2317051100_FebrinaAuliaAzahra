be78a927a0927e7386c3906a123d285a38f98947
febrina1602 <112796775+febrina1602@users.noreply.github.com>
2025-05-08 15:25:25 +0700

    P web Pertemuan 11 login, logout

---
+++ PwebResp11/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PWeb Responsi 11</title>
+</head>
+<body>
+    <form method="post" action="tampil.php">
+        <label for="">Masukkan Nama:</label>
+        <input type="text" name="nama" id=""><br><br>
+        <label for="">Masukkan Usia:</label>
+        <input type="number" name="usia" id=""><br><br>
+        <label for="">Masukkan NPM:</label>
+        <input type="number" name="npm" id=""><br><br>
+        <label for="">Masukkan Alamat:</label>
+        <textarea name="alamat" id=""></textarea>
+
+        <input type="submit">
+    </form>
+</body>
+</html>
---
+++ PwebResp11/tampil.php
@@ -0,0 +1,14 @@
+<?php
+    $nama = $_POST["nama"];
+    $usia = $_POST["usia"];
+    $npm = $_POST["npm"];
+    $alamat = $_POST["alamat"];
+
+    echo"Nama Anda adalah ". $nama;
+    echo"<br>";
+    echo"Usia Anda adalah ". $usia;
+    echo"<br>";
+    echo"NPM Anda adalah ". $npm;
+    echo"<br>";
+    echo"ALamat Anda: ". $alamat;
+?>
